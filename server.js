72a48a98cd51259de9634b0434220a44425a1ff6
Abhisek Mishra <abhisek.mishra@celigo.com>
2025-07-10 10:31:33 +0530

    getting started with mongo db

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
+// const logger = require('./middleware/logger').default;
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 //Route files
 const bootcamps = require('./routers/bootcamps');
@@ -7,7 +10,22 @@ const bootcamps = require('./routers/bootcamps');
 // load env variables
 dotenv.config({path: './config/config.env'});
 
+// Connect to database
+connectDB();
+
 const app = express();
+// app.use(logger);
+
+// Body Parser
+app.use(express.json());
+
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
@@ -15,6 +33,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+const server = app.listen(PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise)=> {
+    console.log(`Error: ${err.message}`);
+    //Close the server and exit the process
+    server.close(()=> process.exit(1));
+});

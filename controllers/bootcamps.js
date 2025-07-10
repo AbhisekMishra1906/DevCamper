2272b93369f61dac98d2501d0ec3137d47c8c3e6
Abhisek Mishra <abhisek.mishra@celigo.com>
2025-07-10 10:31:33 +0530

    getting started with mongo db

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,8 +1,16 @@
+import Bootcamp from "../models/Bootcamp.js";
+
+
 // @desc.      Get all bootcamps
 // @route.     GET /api/v1/bootcamps
 // @access.    Public
-export function getBootCamps(req, res, next) {
-    res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+export async function getBootCamps(req, res, next) {
+    try{
+        const bootcamps = await Bootcamp.find();
+        res.status(200).json({ success: true, data: bootcamps });
+    } catch(err) {
+        res.status(400).json({ success: false});
+    }
 }
 
 // // @desc.      Get all bootcamps
@@ -16,32 +24,63 @@ export function getBootCamps(req, res, next) {
 // @desc.      Get bootcamp by Id
 // @route.     GET /api/v1/bootcamps/:id
 // @access.    Private
-export function getBootCamp(req, res, next) {
-    res
-    .status(200)
-    .json({ success: true, 
-            msg: `Show bootcamp ${req.params.id}` });
+export async function getBootCamp(req, res, next) {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+         res.status(200).json({ success: true, 
+            data: bootcamp });
+    } catch(err) {
+        res.status(400).json({ success: false});
+    }
 }
 
 // @desc.      Create bootcamp
 // @route.     POST /api/v1/bootcamps
 // @access.    Private
-export function createBootCamp(req, res, next) {
-    res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+export async function createBootCamp(req, res, next) {
+  try {
+    console.log("Incoming Request:", req.body);
+
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({ success: true, data: bootcamp });
+  } catch (err) {
+    console.error("Error creating bootcamp:", err.message);
+
+    // Optional: Forward to error middleware
+    res.status(400).json({
+      success: false,
+      error: err.message
+    });
+  }
 }
 
 // @desc.      Update bootcamp by Id
 // @route.     PUT /api/v1/bootcamps/:id
 // @access.    Private
-export function updateBootCamp(req, res, next) {
-    res.status(200).json({ success: true,
-                           msg: `Update bootcamp ${req.params.id}` });
+export async function updateBootCamp(req, res, next) {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+  if(!bootcamp) {
+    return res.status(400).json({ success: false});
+  }
+
+  res.status(200).json({ success: true, data: bootcamp });
 }
 
 // @desc.      Delete bootcamp bu Id
 // @route.     DELETE /api/v1/bootcamps/:id
 // @access.    Private
-export function deleteBootCamp(req, res, next) {
-    res.status(200).json({ success: true, 
-                           msg: `Delete bootcamp ${req.params.id}` });
+export async function deleteBootCamp(req, res, next) {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+        if(!bootcamp) {
+            return res.status(400).json({ success: true, data: {}});
+        }
+    } catch(err) {
+        res.status(400).json({ success: false });
+    }
 }
---
+++ middleware/logger.js
@@ -0,0 +1,10 @@
+
+//@desc  Logging incoming requests.
+const logger = (req, res, next) => {
+    console.log(
+        `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
+    );
+    next();
+}
+
+export default logger;
